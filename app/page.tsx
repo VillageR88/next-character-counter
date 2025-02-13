bf0c2a3186a48847a638ef8ed0b0738c40a24fd0
VillageR88 <villager88pl@gmail.com>
2025-02-13 09:23:21 +0100

    Initial non-state layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <label className="mt-[48px]">
           <textarea
             id="main-text"
-            className="min-h-[200px] w-full resize-none rounded-[12px] border-2 border-[#E4E4EF] bg-[#F2F2F7] p-[20px] text-[20px] leading-[140%] tracking-[-0.6px] text-[#2A2B37] outline-none [transition:background-color_300ms,color_300ms,border_300ms] dark:border-[#404254] dark:bg-[#2A2B37]"
+            className="min-h-[200px] w-full resize-none rounded-[12px] border-2 border-[#E4E4EF] bg-[#F2F2F7] p-[20px] text-[20px] leading-[140%] tracking-[-0.6px] text-[#2A2B37] outline-none [transition:background-color_300ms,color_300ms,border_300ms] dark:border-[#404254] dark:bg-[#2A2B37] dark:text-[#E4E4EF]"
             placeholder={MAIN_INPUT_PLACEHOLDER}
           />
         </label>
@@ -94,10 +94,11 @@ export default function Home() {
             );
           })}
         </ul>
-        <div className="mt-[24px] text-[#12131A] dark:text-[#E4E4EF]">
+        <div className="mt-[24px] flex flex-col gap-[20px] text-[#12131A] dark:text-[#E4E4EF]">
           <h2 className="text-[24px] font-semibold leading-[130#] tracking-[-1px] [transition:color_300ms]">
             {DENSITY_TITLE}
           </h2>
+          <p className="[transition:color_300ms]">No characters found. Start typing to see letter density.</p>
         </div>
       </main>
     </>
